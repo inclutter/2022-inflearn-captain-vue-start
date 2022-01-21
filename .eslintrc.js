94802abca438e3230e65aaa42b9b9f11da3ad594
inclutter <augustman@knou.ac.kr>
2022-01-22 00:14:57 +0900

    Vue configuration files and basic codes addition.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+  ,
+  extends: [
+    'plugin:vue/recommended',
+  ],
+  rules: {
+    'indent': ['error', 2]
+  }
+}
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div> {{ str }} </div>
+    <div
+      :id="uuid"
+      :class="name"
+    >
+      {{ num }}
+    </div>
+    <p> {{ doubleNum }} </p>
+    <div v-if="loading">
+      Loading...
+    </div>
+    <div v-else>
+      test user has been logged in
+    </div>
+    <div v-show="loading">
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataBinding",
+  data: () => ({
+    str: 'hi',
+    num: 10,
+    uuid: 'abc1234',
+    name: 'text-blue',
+    loading: true
+
+  }),
+  computed: {
+    doubleNum: function () {
+      console.log(this.num);
+      return this.num * 2;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
